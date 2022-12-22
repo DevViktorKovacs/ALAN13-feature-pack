ee7b7806579c00dc01696cd19be95671326889e8
Szesan <viktorhoz@protonmail.com>
2022-12-22 15:44:47 +0100

    Steam integration

---
+++ Steam/InteropHelp.cs
@@ -0,0 +1,189 @@
+﻿using System.Runtime.InteropServices;
+using IntPtr = System.IntPtr;
+
+using System.Text;
+using static Steamworks.InteropHelp;
+
+namespace Steamworks
+{
+    public class InteropHelp
+    {
+        public static void TestIfPlatformSupported()
+        {
+
+        }
+
+        public static void TestIfAvailableClient()
+        {
+            TestIfPlatformSupported();
+            if (CSteamAPIContext.GetSteamClient() == System.IntPtr.Zero)
+            {
+                if (!CSteamAPIContext.Init())
+                {
+                    throw new System.InvalidOperationException("Steamworks is not initialized.");
+                }
+            }
+        }
+
+        public static void TestIfAvailableGameServer()
+        {
+            TestIfPlatformSupported();
+            if (CSteamGameServerAPIContext.GetSteamClient() == System.IntPtr.Zero)
+            {
+                if (!CSteamGameServerAPIContext.Init())
+                {
+                    throw new System.InvalidOperationException("Steamworks GameServer is not initialized.");
+                }
+            }
+        }
+
+        // This continues to exist for both 'out string' and strings returned by Steamworks functions.
+        public static string PtrToStringUTF8(IntPtr nativeUtf8)
+        {
+            if (nativeUtf8 == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            int len = 0;
+
+            while (Marshal.ReadByte(nativeUtf8, len) != 0)
+            {
+                ++len;
+            }
+
+            if (len == 0)
+            {
+                return string.Empty;
+            }
+
+            byte[] buffer = new byte[len];
+            Marshal.Copy(nativeUtf8, buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        public static string ByteArrayToStringUTF8(byte[] buffer)
+        {
+            int length = 0;
+            while (length < buffer.Length && buffer[length] != 0)
+            {
+                length++;
+            }
+
+            return Encoding.UTF8.GetString(buffer, 0, length);
+        }
+
+        public static void StringToByteArrayUTF8(string str, byte[] outArrayBuffer, int outArrayBufferSize)
+        {
+            outArrayBuffer = new byte[outArrayBufferSize];
+            int length = Encoding.UTF8.GetBytes(str, 0, str.Length, outArrayBuffer, 0);
+            outArrayBuffer[length] = 0;
+        }
+
+        // This is for 'const char *' arguments which we need to ensure do not get GC'd while Steam is using them.
+        // We can't use an ICustomMarshaler because Unity crashes when a string between 96 and 127 characters long is defined/initialized at the top of class scope...
+
+        public class UTF8StringHandle : Microsoft.Win32.SafeHandles.SafeHandleZeroOrMinusOneIsInvalid
+        {
+            public UTF8StringHandle(string str)
+                : base(true)
+            {
+                if (str == null)
+                {
+                    SetHandle(IntPtr.Zero);
+                    return;
+                }
+
+                // +1 for '\0'
+                byte[] strbuf = new byte[Encoding.UTF8.GetByteCount(str) + 1];
+                Encoding.UTF8.GetBytes(str, 0, str.Length, strbuf, 0);
+                IntPtr buffer = Marshal.AllocHGlobal(strbuf.Length);
+                Marshal.Copy(strbuf, 0, buffer, strbuf.Length);
+
+                SetHandle(buffer);
+            }
+
+            protected override bool ReleaseHandle()
+            {
+                if (!IsInvalid)
+                {
+                    Marshal.FreeHGlobal(handle);
+                }
+                return true;
+            }
+        }
+
+        //public class UTF8StringHandle : IDisposable {
+        //	public UTF8StringHandle(string str) { }
+        //	public void Dispose() {}
+        //}
+
+
+        // TODO - Should be IDisposable
+        // We can't use an ICustomMarshaler because Unity dies when MarshalManagedToNative() gets called with a generic type.
+        public class SteamParamStringArray
+        {
+            // The pointer to each AllocHGlobal() string
+            IntPtr[] m_Strings;
+            // The pointer to the condensed version of m_Strings
+            IntPtr m_ptrStrings;
+            // The pointer to the StructureToPtr version of SteamParamStringArray_t that will get marshaled
+            IntPtr m_pSteamParamStringArray;
+
+            public SteamParamStringArray(System.Collections.Generic.IList<string> strings)
+            {
+                if (strings == null)
+                {
+                    m_pSteamParamStringArray = IntPtr.Zero;
+                    return;
+                }
+
+                m_Strings = new IntPtr[strings.Count];
+                for (int i = 0; i < strings.Count; ++i)
+                {
+                    byte[] strbuf = new byte[Encoding.UTF8.GetByteCount(strings[i]) + 1];
+                    Encoding.UTF8.GetBytes(strings[i], 0, strings[i].Length, strbuf, 0);
+                    m_Strings[i] = Marshal.AllocHGlobal(strbuf.Length);
+                    Marshal.Copy(strbuf, 0, m_Strings[i], strbuf.Length);
+                }
+
+                m_ptrStrings = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(IntPtr)) * m_Strings.Length);
+                SteamParamStringArray_t stringArray = new SteamParamStringArray_t()
+                {
+                    m_ppStrings = m_ptrStrings,
+                    m_nNumStrings = m_Strings.Length
+                };
+                Marshal.Copy(m_Strings, 0, stringArray.m_ppStrings, m_Strings.Length);
+
+                m_pSteamParamStringArray = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(SteamParamStringArray_t)));
+                Marshal.StructureToPtr(stringArray, m_pSteamParamStringArray, false);
+            }
+
+            ~SteamParamStringArray()
+            {
+                if (m_Strings != null)
+                {
+                    foreach (IntPtr ptr in m_Strings)
+                    {
+                        Marshal.FreeHGlobal(ptr);
+                    }
+                }
+
+                if (m_ptrStrings != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(m_ptrStrings);
+                }
+
+                if (m_pSteamParamStringArray != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(m_pSteamParamStringArray);
+                }
+            }
+
+            public static implicit operator IntPtr(SteamParamStringArray that)
+            {
+                return that.m_pSteamParamStringArray;
+            }
+        }
+    }
+}
---
+++ Utility/SteamManager.cs
@@ -0,0 +1,122 @@
+﻿using Steamworks;
+
+namespace ALAN13featurepack.Utility
+{
+    public class SteamManager
+    {
+        protected static bool s_EverInitialized = false;
+
+        protected static SteamManager s_instance;
+        protected static SteamManager Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    return new SteamManager();
+                }
+                else
+                {
+                    return s_instance;
+                }
+            }
+        }
+
+        protected bool m_bInitialized = false;
+        public static bool Initialized
+        {
+            get
+            {
+                return Instance.m_bInitialized;
+            }
+        }
+
+        public virtual void ShutDown()
+        {
+            if (Initialized)
+            {
+                SteamAPI.Shutdown();
+            }
+        }
+
+        public bool Init()
+        {
+            // Only one instance of SteamManager at a time!
+            if (s_instance != null)
+            {
+                return true;
+            }
+            s_instance = this;
+
+            if (s_EverInitialized)
+            {
+                // This is almost always an error.
+                // The most common case where this happens is when SteamManager gets destroyed because of Application.Quit(),
+                // and then some Steamworks code in some other OnDestroy gets called afterwards, creating a new SteamManager.
+                // You should never call Steamworks functions in OnDestroy, always prefer OnDisable if possible.
+                throw new System.Exception("Tried to Initialize the SteamAPI twice in one session!");
+            }
+
+
+            try
+            {
+                // If Steam is not running or the game wasn't started through Steam, SteamAPI_RestartAppIfNecessary starts the
+                // Steam client and also launches this game again if the User owns it. This can act as a rudimentary form of DRM.
+
+                // Once you get a Steam AppID assigned by Valve, you need to replace AppId_t.Invalid with it and
+                // remove steam_appid.txt from the game depot. eg: "(AppId_t)480" or "new AppId_t(480)".
+                // See the Valve documentation for more information: https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
+
+
+                if (!InputProcessor.IsInDebugMode && SteamAPI.RestartAppIfNecessary(new AppId_t(1888130)))
+                {
+                    DebugHelper.PrettyPrintVerbose("Restarting steam...");
+                    //Application.Quit();
+                    return false;
+                }
+            }
+            catch (System.DllNotFoundException e)
+            { // We catch this exception here, as it will be the first occurrence of it.
+                DebugHelper.PrintError("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e);
+
+                //Application.Quit();
+                return false;
+            }
+
+            // Initializes the Steamworks API.
+            // If this returns false then this indicates one of the following conditions:
+            // [*] The Steam client isn't running. A running Steam client is required to provide implementations of the various Steamworks interfaces.
+            // [*] The Steam client couldn't determine the App ID of game. If you're running your application from the executable or debugger directly then you must have a [code-inline]steam_appid.txt[/code-inline] in your game directory next to the executable, with your app ID in it and nothing else. Steam will look for this file in the current working directory. If you are running your executable from a different directory you may need to relocate the [code-inline]steam_appid.txt[/code-inline] file.
+            // [*] Your application is not running under the same OS user context as the Steam client, such as a different user or administration access level.
+            // [*] Ensure that you own a license for the App ID on the currently active Steam account. Your game must show up in your Steam library.
+            // [*] Your App ID is not completely set up, i.e. in Release State: Unavailable, or it's missing default packages.
+            // Valve's documentation for this is located here:
+            // https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
+            m_bInitialized = SteamAPI.Init();
+
+            if (!m_bInitialized)
+            {
+                DebugHelper.PrintError("[Steamworks.NET] SteamAPI_Init() failed. Refer to Valve's documentation or the comment above this line for more information.");
+
+                return false;
+            }
+
+            s_EverInitialized = true;
+
+            DebugHelper.PrettyPrintVerbose($"Steam connected for {SteamFriends.GetPersonaName()}");
+
+            return true;
+        }
+
+        public virtual void Update()
+        {
+            if (!m_bInitialized)
+            {
+                return;
+            }
+
+            // Run Steam client callbacks
+            SteamAPI.RunCallbacks();
+        }
+    }
+}
